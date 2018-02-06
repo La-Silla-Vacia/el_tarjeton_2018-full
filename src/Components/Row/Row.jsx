cc658fe4f3ef676c1f0b6d351429a19959900bc7
Wietse Neven <hello@wietseneven.com>
2018-02-06 20:56:40 +0100

    Removed imgix integration

--- src/Components/Row/Row.jsx
+++ src/Components/Row/Row.jsx
@@ -55,7 +55,7 @@ export default class Row extends Component {
         onMouseLeave={this.handleMouseLeave}
       >
         {(foto) ?
-          <image xlinkHref={`${foto}?w=40&h=40`} x={0} y={0} height={20} width={20} />
+          <image xlinkHref={`${foto}`} x={0} y={0} height={20} width={20} />
           : false}
         <rect className={s.color} width={20} height={20} x={0} y={0} data-partido={partido} />
       </g>
